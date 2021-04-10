b87112a762afd1e23ab8e0ec748170502d77d0c2
aditya <adityavadityav@gmail.com>
2021-04-10 23:15:52 +0530

    Testing PWA

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import Router from './Router'
 import { DBConfig } from './db/DBConfig';
 import { initDB } from 'react-indexed-db';
-
+import './assets/style/bootstrap.min.css'
 //Initialized IndexedDB
 initDB(DBConfig);
 function App() {
